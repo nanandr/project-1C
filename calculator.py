95af8b6bd6fbaf128ade03b0e7463173a45898e6
nanand <61643931+nanandr@users.noreply.github.com>
2024-11-07 14:28:46 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
-print("Sum of integers")
+print("Dividing integers")
 a = []
+res = 1
 while True:
   i = int(input("Enter number: "))
   if i < 0:
     break
   a.append(i)
 
-print(f"Total: {sum(a)}")
+for val in a:
+  res = res / val
+
+print(f"Total: {res}")
