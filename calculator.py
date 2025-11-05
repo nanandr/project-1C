5d3a901f7f462d8505c6686ea2306cdb9eb88b8d
nanand <61643931+nanandr@users.noreply.github.com>
2025-11-05 11:32:40 +0700

    bagi
    Refactor calculator to divide two predefined numbers instead of user input.

--- calculator.py
+++ calculator.py
@@ -1,13 +1,4 @@
-print("Dividing integers")
-a = []
-res = 1
-while True:
-  i = int(input("Enter number: "))
-  if i < 0:
-    break
-  a.append(i)
+a = 5
+b = 10
 
-for val in a:
-  res = res / val
-
-print(f"Total: {res}")
+print(a/b)
